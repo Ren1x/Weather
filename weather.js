b1d4ea980c794d06b33ca126eb749e9872ceb2a7
ren1x <ren1x@yandex.ru>
2018-04-30 15:42:14 +0300

    done

--- weather.js
+++ weather.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+	// Для проверки с чего зашел пользователь
+	const isMobile = window.matchMedia("only screen and (max-width: 760px)");
 	const $userInput = $("#userInput");
 	// общий блок погоды
 	const $sectionContainer = $("#sectionContainer");
@@ -29,23 +31,28 @@ $(document).ready(() => {
 				return jsonResponse;
 			}
 		} catch (error) {
-			alert(error);
+			err(error);
 		}
 	};
 
 	const createBlockOfWeather = weatherData => {
-		//для сокращения кода в переборе следующей функции
-		let days = weatherData.forecast.forecastday;
+		//очищаем блоки с погодой
+		$section.forEach(section => section.empty());
 
 		//Создание блоков погоды с днями недели
-		addWeatherOnBlock(weatherData, days);
-		// Блок с погодой появляется
-		$sectionContainer.show(500);
+		addWeatherOnBlock(weatherData);
 
+		// Блок с погодой появляется
+		$sectionContainer.show(300);
+
+		// Возвращаем текст погоды для changeBackground()
 		return weatherData.current.condition.text;
 	};
 
-	const addWeatherOnBlock = (weatherData, days) => {
+	const addWeatherOnBlock = weatherData => {
+		//для сокращения кода в переборе следующей функции
+		let days = weatherData.forecast.forecastday;
+
 		$section.forEach((section, i) => {
 			let info;
 			// основной блок?
@@ -60,7 +67,7 @@ $(document).ready(() => {
 						"</p>" +
 						"<p> <span class='temperature'>" +
 						days[i].day.maxtemp_c +
-						"&deg; / " +
+						"&deg; </span> / <span class='temperature'>" +
 						days[i].day.mintemp_c +
 						"&deg;</span></p>" +
 						"<h6>" +
@@ -77,23 +84,23 @@ $(document).ready(() => {
 			"<div class='container'>" +
 			"<div class='row'>" +
 			"<div class='col-md-6 col-6 verticalHt'>" +
-			"<p>Сейчас <span class='temperature'>" +
+			"<p><span class='temperature'>" +
 			weatherData.current.temp_c +
 			"&deg;</span></p>" +
 			"<img src='http:" +
 			weatherData.current.condition.icon +
 			"'></img>" +
-			"<p class='d-none d-sm-block'>" +
+			"<p class='d-sm-block'>" +
 			weatherData.current.condition.text +
 			"</p>" +
-			"<p>По ощущению " +
+			"<p>Сейчас по ощущению " +
 			weatherData.current.feelslike_c +
 			"&deg;</p>" +
 			"</div>" +
 			"<div class='col-md-6 col-6'>" +
-			"<p> Сегодня <span class='temperature'> " +
+			"<p class='text-nowrap'><span class='temperature'> " +
 			days[0].day.maxtemp_c +
-			"&deg;/ " +
+			"&deg;</span> / <span class='temperature'>" +
 			days[0].day.mintemp_c +
 			"&deg;</span></p>" +
 			'<img src="http:' +
@@ -102,7 +109,7 @@ $(document).ready(() => {
 			"<p>" +
 			days[0].day.condition.text +
 			"</p>" +
-			"<h6>" +
+			"<h6>Сегодня " +
 			$dayOfWeek[new Date().getDay()] +
 			" (" +
 			days[0].date.slice(5).replace("-", ".") +
@@ -113,39 +120,57 @@ $(document).ready(() => {
 		return currentDay;
 	};
 
+	// Меняет фон в зависимости от погоды
 	const changeBackground = condition => {
 		$("body").removeClass();
+
+		let mobile = "";
+
+		//Проверяем с чего зашел пользователь
+		if (isMobile.matches) {
+			mobile = "mobile_";
+		}
+
 		let newCondition = condition.toLowerCase();
+
 		if (
 			~newCondition.indexOf("дожд") ||
 			~newCondition.indexOf("гроз") ||
 			~newCondition.indexOf("морось")
 		) {
-			$("body").addClass("rain");
+			$("body").addClass(mobile + "rain");
 		} else if (~newCondition.indexOf("облачн")) {
-			$("body").addClass("cloudy");
+			$("body").addClass(mobile + "cloudy");
 		} else if (
 			~newCondition.indexOf("ясн") ||
 			~newCondition.indexOf("солнечн")
 		) {
-			$("body").addClass("sun");
+			$("body").addClass(mobile + "sun");
+		} else if (
+			~newCondition.indexOf("сне") ||
+			~newCondition.indexOf("метел")
+		) {
+			$("body").addClass(mobile + "snow");
 		} else {
 			$("body").addClass("defaultWeather");
 		}
+
+		mobile = "";
 	};
 
-	function executeSearch() {
-		$sectionContainer.hide(500);
-		$section.forEach(section => section.empty());
+	const err = e => {
+		if (e.name === "TypeError") {
+			$userInput.addClass("is-invalid");
+		} else {
+			alert("Ошибка соединения");
+		}
+	};
 
-		weather()
-			.then(weather => createBlockOfWeather(weather))
-			.then(condition => changeBackground(condition));
-
-		nameOfCity();
-
-		return false;
-	}
+	//Прячет блок с погодой и убирает ошибку ввода(если есть)
+	const clearStyle = () => {
+		$userInput.removeClass("is-invalid");
+		$sectionContainer.hide(700);
+	};
 
 	const nameOfCity = () => {
 		let cityName =
@@ -157,5 +182,22 @@ $(document).ready(() => {
 		$userInput.val("");
 	};
 
+	const executeSearch = () => {
+		clearStyle();
+
+		weather()
+			.then(weather => createBlockOfWeather(weather))
+			.then(condition => changeBackground(condition))
+			.catch(e => err(e));
+
+		nameOfCity();
+
+		return false;
+	};
+
 	$search.click(executeSearch);
 });
+
+// if(isMobile.matches) {
+// 	alert('fddf');
+// }
